49f7dda4274376cdbddea8f5e9d9d75db9ac968a
Michael Isip <michael.isip.work@gmail.com>
2024-11-19 21:37:59 +0800

    fix(migrations): remove dropColumn username that causes error on migrate refresh

--- database/migrations/2024_11_18_195043_add_username_field_on_users_table.php
+++ database/migrations/2024_11_18_195043_add_username_field_on_users_table.php
@@ -21,8 +21,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('username');
-        });
     }
 };
