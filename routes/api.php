e25f094ae04dfa93d6799310789f423f68d19249
Michael Isip <michael.isip.work@gmail.com>
2024-11-20 20:48:19 +0800

    chore(health-check): improve health check endpoint api response

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\Authentication\LoginController;
 use App\Http\Controllers\Authentication\RegisterController;
 use App\Http\Controllers\VoucherController;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', function(Request $request) {
+Route::get('/health-check', function(Request $request) {
     return response()->json([
-        'message' => 'Ok'
+        'status' => 'OK',
+        'timestamp' => Carbon::now()->format('F j, Y, g:i A')
     ]);
 });
 
